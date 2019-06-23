5a28b07a72fd4f2bb7c9dccb2d4b3ac5a7188f32
Christopher Getschmann <christopher@volzo.de>
2019-06-23 21:33:09 +0200

    bugfix polygon closing

--- dtm/rtest.py
+++ dtm/rtest.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append("..")
 from svgwriter import SvgWriter
 
+from datetime import datetime
+
 import rasterio
 import rasterio.features
 import rasterio.warp
@@ -22,9 +24,14 @@ import cv2
 DB_NAME = "import"
 DB_PREFIX = "osm_"
 
-CITY = "Weimar"
+TIMER_STRING = "{:<50s}: {:2.2f}s"
 
-NUM_ELEVATION_LINES = 50
+CITY = "Weimar"
+NUM_ELEVATION_LINES = 15
+
+MAP_WIDTH = 100
+MAP_SIZE = [1000, 1000]
+MAP_SCALE = MAP_SIZE[0] / float(MAP_WIDTH)
 
 conn = psycopg2.connect(database='osm', user='osm')
 curs = conn.cursor()
@@ -57,6 +64,30 @@ def generate_elevation_lines(image):
 
     return lines
 
+def simplify_polygons(polygons):
+    polygons_simplified = []
+
+    for poly in polygons:
+        new_poly = []
+
+        if len(poly) < 20:
+            continue
+
+        if len(poly) < 30:
+            polygons_simplified.append(poly)
+            continue
+
+        polygons_simplified.append(poly)
+
+        # for i in range(0, len(poly)):
+        #     if i%2 == 0:
+        #         new_poly.append(poly[i])
+
+        # polygons_simplified.append(new_poly)
+
+    return polygons_simplified
+
+timer_start = datetime.now()
 curs.execute("""
     SELECT geometry 
     FROM {0}.{1}admin 
@@ -73,8 +104,10 @@ project = partial(
     pyproj.Proj(init='epsg:3044')) # destination coordinate system
 
 shape = [transform(project, shape)]
+print(TIMER_STRING.format("load shape data", (datetime.now()-timer_start).total_seconds()))
 
-with rasterio.open('thueringen_20m.tif') as dataset:
+timer_start = datetime.now()
+with rasterio.open('thueringen_50m.tif') as dataset:
 
     # print("indexes: {}".format(dataset.indexes))
     # print("size: {} x {}".format(dataset.width, dataset.height))
@@ -86,14 +119,22 @@ with rasterio.open('thueringen_20m.tif') as dataset:
     # plt.matshow(band)
     # plt.savefig("data1.png")
 
-    out_image, out_transform = mask.mask(dataset, shape, crop=True)
-    # out_meta = dataset.meta.copy()
-    # reduce the image dimensions from (1, x, y) to (x, y)
-    band = np.squeeze(np.asarray(out_image))
+    # out_image, out_transform = mask.mask(dataset, shape, crop=True)
+    # # out_meta = dataset.meta.copy()
+    # # reduce the image dimensions from (1, x, y) to (x, y)
+    # band = np.squeeze(np.asarray(out_image))
 
-    # band = dataset.read(1)
+    band = dataset.read(1)
+
+    print(TIMER_STRING.format("load raster data", (datetime.now()-timer_start).total_seconds()))
+    timer_start = datetime.now()
 
     band = band.clip(min=0)
+
+    MAP_SIZE = (band.shape[1], band.shape[0])
+    MAP_SCALE = float(MAP_WIDTH) / MAP_SIZE[0]
+
+    print(MAP_SCALE)
 
     min_elevation = np.min(band[band > 0])
     max_elevation = np.max(band)
@@ -107,18 +148,42 @@ with rasterio.open('thueringen_20m.tif') as dataset:
         elevation_lines.append(contours)
 
     print("data range, min: {:.2f} | max: {:.2f}".format(min_elevation, max_elevation))
+    print(TIMER_STRING.format("process elevation lines", (datetime.now()-timer_start).total_seconds()))
 
     # plt.imshow(band)
     # plt.savefig("data1.png")
 
-MAP_SIZE = (1000, 1000)
-svg = SvgWriter("elevation_lines.svg", MAP_SIZE)
+svg = SvgWriter("elevation_lines.svg", (MAP_SIZE[0]*MAP_SCALE, MAP_SIZE[1]*MAP_SCALE))
 
+timer_start = datetime.now()
+
+polys = []
 for height_level in elevation_lines:
     for line in height_level:
         coords = []
         for pair in line:
             coords.append([pair[0][0], pair[0][1]])
-        svg.add_polygon(coords, opacity=0.0)
+
+        # close the polygon
+        coords = coords + [[coords[0][0], coords[0][1]]]
+
+        polys.append(coords)
+
+polygons_simplified = simplify_polygons(polys)
+
+for poly in polygons_simplified:
+    for i in range(0, len(poly)):
+        poly[i][0] *= MAP_SCALE
+        poly[i][1] *= MAP_SCALE
+
+print(TIMER_STRING.format("simplify polygons", (datetime.now()-timer_start).total_seconds()))
+
+timer_start = datetime.now()
+for poly in polygons_simplified:
+    svg.add_polygon(poly, stroke_width=0.25, opacity=0) # , opacity=0.02
+
+print(TIMER_STRING.format("loading svgwriter", (datetime.now()-timer_start).total_seconds()))
+
+print("map size: {:.2f} x {:.2f}".format(MAP_SIZE[0]*MAP_SCALE, MAP_SIZE[1]*MAP_SCALE))
 
 svg.save()
